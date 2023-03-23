e450a6f1f5759e96ff5cfcd6104e99ac5645771e
pooranjoyb <pooranjoyb2016@gmail.com>
2023-03-24 02:30:22 +0530

    Added Animations and added new content

--- public/tailwind.config.js
+++ public/tailwind.config.js
@@ -3,21 +3,7 @@ module.exports = {
   content: ["*.{html,js,ejs}"],
   theme: {
     screens: {
-      'mobile': { 'max': '720px' },
-    },
-    extend: {
-      animation: {
-        fadeIn: "fadeIn 2s ease-in forwards"
-      },
-      keyframes: {
-        fadeIn: {
-          "0%": { opacity: 0 },
-          "100%": { opacity: 1 }
-        }
-      },
-    },
-    variants: {
-      animation: ["motion-safe"]
+      'mobile': { 'max': '1300px' },
     },
   },
   plugins: [],
