133e205073041b1009f54e70ec08573c2c0bd80b
pooranjoyb <pooranjoyb2016@gmail.com>
2023-05-28 22:40:33 +0530

    Rendered teamData from JSON

--- backend/Routes/teamRoute.js
+++ backend/Routes/teamRoute.js
@@ -1,10 +1,10 @@
 import express from 'express'
-
+import members from '../assets/team.json' assert {type: 'json'}
 const teamRoute = express.Router();
 
 //Teams Route
 teamRoute.get('/team', (req, res) => {
-    res.render('../public/team')
+    res.render('../public/team', {members1: members.row1, members2: members.row2})
 })
 
 export default teamRoute;
