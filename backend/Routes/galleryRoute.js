6f1aea0d6387a86ebad804d423a1e82c9104e884
pooranjoyb <pooranjoyb2016@gmail.com>
2023-07-23 00:12:26 +0530

    Setup params for gallery

--- backend/Routes/galleryRoute.js
+++ backend/Routes/galleryRoute.js
@@ -1,10 +1,11 @@
 import express from 'express'
+import artworks from '../assets/artworks.json' assert {type: 'json'}
 
 const galleryRoute = express.Router();
 
 //Gallery Route
 galleryRoute.get('/gallery', (req, res) => {
-    res.render('../public/gallery')
+    res.render('../public/gallery', {modern: artworks.modern, portrait: artworks.portrait})
 })
 
 export default galleryRoute;
