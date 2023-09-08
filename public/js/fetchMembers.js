8f877ce1ca229c5ce41d770bdb4b2a1fdd17c97b
pooranjoyb <pooranjoyb2016@gmail.com>
2023-09-09 02:20:25 +0530

    Updated Team

--- public/js/fetchMembers.js
+++ public/js/fetchMembers.js
@@ -6,7 +6,7 @@ const fetchMembers = () => {
     .then((data) => {
         data.forEach((item) => {
             let li = document.createElement("li");
-            li.innerText = item.name;
+            li.innerText = item.Name;
             li.classList.add("text-body-color", "mb-4", "flex", "text-base", "p-4")
             members.appendChild(li);
         });
